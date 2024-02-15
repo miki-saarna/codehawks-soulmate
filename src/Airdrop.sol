c2904c8156caec73d07e523a28e0010e90a568ec
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-02-14 16:46:08 -0800

    Syntax fix

--- src/Airdrop.sol
+++ src/Airdrop.sol
@@ -79,7 +79,7 @@ contract Airdrop {
         if (
             tokenAmountToDistribute >= airdropVaultBalance
         ) {
-            tokenAmountToDistribute = airdropVaultBalance
+            tokenAmountToDistribute = airdropVaultBalance;
         }
         _claimedBy[msg.sender] += tokenAmountToDistribute;
 
